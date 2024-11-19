120e2c3f0d5e838d4ba2c77664b1210162e96ff5
qqqquuuuuuaaaadddddddd <email@exmpl.com>
2024-11-19 23:31:20 +0600

    PlayerStats.cs

---
+++ Code/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿[Icon("favorite")]
+public class PlayerStats : Component
+{
+	[Property, Group("Variables")]
+	public float Health { get; set; } = 100f;
+	[Property, Group("Variables")]
+	public float MaxHealth { get; set; } = 100f;
+	[Property, Group("Variables")]
+	public float Resistance { get; set; } = 0f;
+}
